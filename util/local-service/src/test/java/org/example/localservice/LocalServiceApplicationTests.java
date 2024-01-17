e586589d8cf32ffb66193b76575dee718599f63f
Obi <obi@bartinsights.com>
2024-01-17 01:41:26 +0000

    Adding local-service util

---
+++ util/local-service/src/test/java/org/example/localservice/LocalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.localservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
